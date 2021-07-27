58d850c4696b225ed0ec817ee04598f2d90c5d17
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-07-27 16:01:19 -0300

    Added main class to run the project

---
+++ src/main/java/com/example/advancedsearch/SpringRestAdvancedSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAdvancedSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAdvancedSearchApplication.class, args);
+	}
+
+}
