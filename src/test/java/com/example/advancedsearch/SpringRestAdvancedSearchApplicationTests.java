7aa600ef05932b77492cce61dbb023d9e5976757
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-07-27 15:28:07 -0300

    Default test structure

---
+++ src/test/java/com/example/advancedsearch/SpringRestAdvancedSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAdvancedSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
